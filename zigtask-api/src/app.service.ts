a63ae70146129fe142213fc1221c51e90503cd98
giakhang94 <ngk.khang94@gmail.com>
2025-06-18 17:55:35 +0700

    feat: #6 - inject usser repository in usersService

--- zigtask-api/src/app.service.ts
+++ zigtask-api/src/app.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }

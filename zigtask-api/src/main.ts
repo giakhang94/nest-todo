4835c73eb25163e5aa71ac65724b6e5edd616388
giakhang94 <ngk.khang94@gmail.com>
2025-06-18 21:45:13 +0700

    feat: #10 - be - swagger - createUser api

--- zigtask-api/src/main.ts
+++ zigtask-api/src/main.ts
@@ -10,8 +10,6 @@ async function bootstrap() {
     .setTitle('Zigvy task homework docs')
     .setDescription('The task API documentation')
     .setVersion('1.0')
-    .addTag('Tasks')
-    .addTag('Users')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);

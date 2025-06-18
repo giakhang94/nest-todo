0a300c5cab3e789bfea749cfaf395c43eb63c517
giakhang94 <ngk.khang94@gmail.com>
2025-06-19 01:05:03 +0700

    feat: #16 - login user, swagger, token

--- zigtask-api/src/auth/auth.module.ts
+++ zigtask-api/src/auth/auth.module.ts
@@ -3,10 +3,25 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          global: true,
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: configService.get<string>('JWT_EXP') },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AuthModule {}

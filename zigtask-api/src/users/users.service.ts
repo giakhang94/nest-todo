07d4bbe74f1971b622d1e12a944e48dcc021dff4
giakhang94 <ngk.khang94@gmail.com>
2025-06-18 18:39:04 +0700

    feat: #8 - be - config TypeOrmModule, user Repository

--- zigtask-api/src/users/users.service.ts
+++ zigtask-api/src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+}
